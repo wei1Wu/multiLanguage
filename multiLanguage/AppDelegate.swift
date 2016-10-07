ae0c599d2f42630d24d103fe0109cf1b6522fdf5
davissonwu <1174846945@qq.com>
2016-10-07 23:41:17 +0800

    ceshi

---
+++ multiLanguage/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  multiLanguage
+//
+//  Created by davissonwu on 16/10/7.
+//  Copyright © 2016年 davissonwu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
